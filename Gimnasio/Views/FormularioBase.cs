8b578faf93672723cf7c6b2c39aee64b740f323d
N3roAstar0th <nahuelor.org@gmail.com>
2024-10-23 17:33:57 +0200

    Añadir clase FormularioBase en Gimnasio.Views
    Se ha añadido un nuevo archivo `FormularioBase.Designer.cs` que contiene la definición parcial de la clase `FormularioBase`, incluyendo la declaración de un contenedor de componentes (`components`), el método `Dispose` y el método `InitializeComponent`.
    
    También se ha añadido un nuevo archivo `FormularioBase.cs` que contiene la definición parcial de la clase `FormularioBase`, que hereda de `Form`. En el constructor de la clase, se llama al método `InitializeComponent` y se configura el formulario para que se ejecute en pantalla completa (`WindowState = FormWindowState.Maximized`) y sin bordes (`FormBorderStyle = FormBorderStyle.None`).

---
+++ Gimnasio/Views/FormularioBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gimnasio.Views
+{
+    public partial class FormularioBase : Form
+    {
+        public FormularioBase()
+        {
+            InitializeComponent();
+            // Configurar el formulario para que se ejecute en pantalla completa
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+    }
+}
+
